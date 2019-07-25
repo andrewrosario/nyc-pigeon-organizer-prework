b68936ebd3f18396012ecf27836f47c2b8f0fe58
Andrew Rosario <rosario.andrew@gmail.com>
2019-07-25 23:37:16 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_names = []
+  pigeon_characteristics = []
+  # Get Pigeon Names
+  data.each do |characteristic, category|
+    # Get Characteristics
+    pigeon_characteristics << 
+    category.each do |type, names|
+      pigeon_names << names
+      pigeon_names = pigeon_names.flatten.uniq
+    end
+  end
+  
+  
+  
+  
 end
