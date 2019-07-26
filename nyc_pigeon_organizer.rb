f460d1071f2161296ed57f734822a4cc115aefbd
Andrew Rosario <rosario.andrew@gmail.com>
2019-07-26 00:04:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -19,7 +19,7 @@ def nyc_pigeon_organizer(data)
       pigeon_names.each do |nam|
         pigeon_characteristics.each do |char|
           puts names
-            if names.include?(nam) && !pigeon_list[nam].key
+            if names.include?(nam) && !pigeon_list[nam].key?(char)
               pigeon_list[nam] = {char => type}
             else
               # pigeon_list[nam][char] = type
